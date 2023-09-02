99abc66b2b1315e6d1e36103150287fcb6402c11
Carlos Eduardo <cadudevmaster@gmail.com>
2023-09-02 12:32:11 -0300

    feat: ubuntu_custom

--- ubuntu_custom.sh
+++ ubuntu_custom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+
+ssh-keygen -f ~/sshkey -t rsa -b 4096
+
 docker build --build-arg SUDO_PASSWORD=carlos -t ubuntu_custom .
 docker run -d -p "2222:22" -p "8000:80" -p "3333:3306" ubuntu_custom
-
-#generate sshkeys
-ssh-keygen -f ~/sshkey -t rsa -b 4096
